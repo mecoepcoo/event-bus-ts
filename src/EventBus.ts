62c5102be54b7f5c4fba4afe14ad31e2462ed48a
mecoepcoo <340349237@qq.com>
2021-04-01 22:37:03 +0800

    feat: 新增一次性订阅和退订全部事件功能

--- src/EventBus.ts
+++ src/EventBus.ts
@@ -1,16 +1,21 @@
 import { Event, ObjLike } from './types'
 
 export default class EventBus {
-  private events: Event[] = []
-  private uid = 0
+  private _events: Event[] = []
+  private _uid = 0
+  
+  get events() {
+    return this._events
+  }
+
   /**
    * 注册/订阅事件
    * @param name 事件名
    * @param handler 执行函数
    */
   register(name: string, handler: Function): number {
-    const eventId = this.uid + 1
-    const events = this.events
+    const eventId = this._uid + 1
+    const events = this._events
     const event = this.findEvent(name)
 
     if (event !== null) {
@@ -30,12 +35,25 @@ export default class EventBus {
     return eventId
   }
   /**
+   * 注册事件，触发一次后移除
+   * @param name 事件名
+   * @param execute 执行函数
+   */
+   registerOnce(name: string, execute: Function) {
+    const _this = this
+    const eventId = this.register(name, handler)
+    function handler() {
+      _this.unregister(name, eventId)
+      execute.apply(null, arguments)
+    }
+  }
+  /**
    * 退订事件
    * @param name 事件名称
    * @param eventId 事件id
    */
   unregister(name: string, eventId: number): EventBus {
-    const events = this.events
+    const events = this._events
 
     for (const i in events) {
       if (name === events[i].name) {
@@ -53,12 +71,18 @@ export default class EventBus {
     return this
   }
   /**
+   * 退订全部事件
+   */
+   unregisterAll() {
+    this._events = []
+  }
+  /**
    * 发射事件
    * @param name 事件名称
    * @param payload 荷载
    */
   emit(name: string, payload: ObjLike): EventBus {
-    const events = this.events
+    const events = this._events
 
     for (const i in events) {
       if (name === events[i].name) {
@@ -77,7 +101,7 @@ export default class EventBus {
   }
 
   private findEvent(name: string): Event | null {
-    const events = this.events
+    const events = this._events
     for (const i in events) {
       if (name === events[i].name) {
         return events[i]
